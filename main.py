e371e17109fe8e35f1f30343e8173cd525256a93
lstorm-ux <63728542+lstorm-ux@users.noreply.github.com>
2020-05-20 09:56:35 +0200

    fizzbuzz

---
+++ main.py
@@ -0,0 +1,14 @@
+number = int(input("Select a number between 1 and 100:"))
+
+for fizzbuzz in range(1, number+1):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
+
